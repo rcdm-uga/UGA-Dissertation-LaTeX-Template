f6494169336cd7a250d3a3a42972da107791c098
Caleb Crumley <calebcrumley@lkings-iMac.local>
2020-02-03 14:56:22 -0500

    adding citations

--- clean.sh
+++ clean.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 #Cleaning up my LaTeX Projects
 
-rm *.aux *.bbl *.blg *.dvi *.fdb_latexmk *.fls *.lof *.log *.lot *.gz *.toc *.out 
+rm *.aux *.bbl *.blg *.dvi *.fdb_latexmk *.fls *.lof *.log *.lot *.gz *.toc *.out *.xml *.bcf 
